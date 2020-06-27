431b9f655b21e9e859e42d2cf4b8e227531c17f0
vishnu <vishnu@192.168.0.133>
2020-06-27 14:47:52 +0530

    Change-Id: I0000000000000000000000000000000000000000 ClusteringFactor
    Change-Id: Ib24409943c8dc831a16815ea613c1d8e928ad367

---
+++ PTFinal/src/Generic/ClusteringFactor.java
@@ -0,0 +1,130 @@
+package Generic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ClusteringFactor {
+
+	@SuppressWarnings("static-access")
+	public void runBatchLoad() {
+		try {
+			ExecutorService asd = Executors.newFixedThreadPool(10);
+			int i = 0;
+			while (i < 10) {
+				asd.submit(new BatchLoad());
+				i++;
+			}
+			asd.shutdown();
+			
+			while(!asd.isTerminated()) {
+				Thread.currentThread().sleep(1000);
+				System.out.print(".");
+			}
+			asd.shutdownNow();
+		}
+		catch(Exception E) {
+			E.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("static-access")
+	public void runClassicLoad() {
+		try {
+			ExecutorService asd = Executors.newFixedThreadPool(10);
+			int i = 0;
+			while (i < 10) {
+				asd.submit(new ClassicLoad());
+				i++;
+			}
+			asd.shutdown();
+			
+			while(!asd.isTerminated()) {
+				Thread.currentThread().sleep(1000);
+				System.out.print(".");
+			}
+			asd.shutdownNow();
+		}
+		catch(Exception E) {
+			E.printStackTrace();
+		}
+	}
+	
+	class BatchLoad implements Runnable{
+		public void run() {
+			try {
+				Connection oraCon = DBConnection.getOraConn();
+				PreparedStatement pstmt = oraCon.prepareStatement("insert into clustering(student_id, dept_id, marks) values (ora.nextval, ?,?)");
+				int i = 0;
+				while (i < 5000) {
+					pstmt.setInt(1, OraRandom.randomUniformInt(100));
+					pstmt.setInt(1, OraRandom.randomSkewInt(100));
+					pstmt.addBatch();
+					if (i/100 == 0) {
+						pstmt.executeBatch();
+					}
+					i++;
+				}
+				pstmt.executeBatch();
+				pstmt.close();
+				oraCon.close();
+				
+			}
+			catch(Exception E) {
+				E.printStackTrace();
+			}
+		}
+	}
+	
+	class ClassicLoad implements Runnable{
+		public void run() {
+			try {
+				Connection oraCon = DBConnection.getOraConn();
+				PreparedStatement pstmt = oraCon.prepareStatement("insert into clustering(student_id, dept_id, marks) values (ora.nextval, ?,?)");
+				int i = 0;
+				while (i < 5000) {
+					pstmt.setInt(1, OraRandom.randomUniformInt(100));
+					pstmt.setInt(1, OraRandom.randomSkewInt(100));
+					pstmt.executeUpdate();
+					i++;
+				}
+				pstmt.close();
+				oraCon.close();
+			}
+			catch(Exception E) {
+				E.printStackTrace();
+			}
+		}
+	}
+	
+	void createTable() {
+		try {
+			Connection oraCon = DBConnection.getOraConn();
+			Statement stmt = oraCon.createStatement();
+			String SQL;
+			try {
+				SQL = "drop table clustering";
+				stmt.execute(SQL);
+			}
+			catch(Exception E) {}
+			try {
+				SQL = "drop sequence ora";
+				stmt.execute(SQL);
+			}
+			catch(Exception E) {}
+			SQL = "create sequence ora start with 1 increment by 1";
+			stmt.execute(SQL);
+			SQL = "create table clustering (student_id number, dept_id number, marks number)";
+			stmt.execute(SQL);
+			SQL = "alter table clustering add constraint clustering_pk primary key(student_id)";
+			stmt.execute(SQL);
+			stmt.close();
+			oraCon.close();
+		}
+		catch(Exception E) {
+			E.printStackTrace();
+		}
+	}
+}
