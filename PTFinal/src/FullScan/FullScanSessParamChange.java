dd9d17a8ef0e03c81752f106b1bc65387f9b8caa
vishnu <vishnu@localhost>
2020-07-20 18:06:56 +0530

    Change-Id: I0000000000000000000000000000000000000000 Index stages!
    Change-Id: I9da776a19736c15f38eeda123c2cc9e32c68e810

---
+++ PTFinal/src/FullScan/FullScanSessParamChange.java
@@ -0,0 +1,28 @@
+package FullScan;
+import java.sql.Statement;
+
+import Generic.DBConnection;
+
+import java.sql.Connection;
+
+public class FullScanSessParamChange implements Runnable{
+	public void run() {
+		try {
+			Connection oraCon = DBConnection.getOraConn();
+			Statement stmt = oraCon.createStatement();
+			String SQL = "alter session set db_file_multiblock_read_count = 31";
+			stmt.execute(SQL);
+			SQL = "select avg(mark1) from students";
+			int i = 0;
+			while (i < 100000) {
+				stmt.executeQuery(SQL);
+				i++;
+			}
+			
+		}
+		catch(Exception E) {
+			E.printStackTrace();
+		}
+	}
+	
+}
